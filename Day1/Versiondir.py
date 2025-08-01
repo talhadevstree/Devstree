364850a49b39042d5fa138cf5be9a68621ee5445
talhadevstree <talha.s@devstree.in>
2025-08-01 11:24:41 +0530

    commit

--- Day1/Versiondir.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-
-print({"Version is:"+sys.version})
-print({"Directory is:"+os.getcwd()})
--- Day3/TypeConversion.py
+++ Day3/TypeConversion.py
@@ -1,14 +1,13 @@
 # #str to int
 str1 = "555"
 print("From string to integer:",int(str1))
-
 # # #int to float
 
-int1 = 34
+int1 = 33
 print("From Integer to Float:",float(int1))
 
 # # int to string
-    
+
 int2 = "hello"
 strv = str(int2)
 print("From Integer to String:",strv)

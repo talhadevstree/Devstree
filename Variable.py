a5cfb6bac2b630fc2199e7ff93a752c50952232b
talhadevstree <talha.s@devstree.in>
2025-08-01 12:00:20 +0530

    commit

--- Variable.py
+++
@@ -1,9 +0,0 @@
-string = "HelloWorld"
-int = 123
-float = 10.3
-
-print("Type is ",type(string), "Value is", string)
-print("Type is ",type(int) ,"Value is", int)
-print("Type is ",type(float) ,"Value is", float)
-
-

bdf83e78093527c69eb36461d621656828658863
talhadevstree <talha.s@devstree.in>
2025-08-04 12:47:17 +0530

    Initial commit

---
+++ Week2/W2Day2/Phonebook.py
@@ -0,0 +1,23 @@
+phonebook = []
+while True:
+    choose = input("You Want to store 'Data' or 'Display':")
+
+    if choose == 'Data':
+        name = input("Enter a Name:")
+        phone = int(input("Enter a Number:"))
+        phonebook.append(name )
+        phonebook.append(phone)
+        print("########################")
+        print("Item Added in Phone Book")
+        print("########################")
+    elif choose == 'Display':
+        print(phonebook)
+
+
+    choice = input("You want to co continue? : 'Y' or 'N':")
+
+    if choice == 'Y':
+        print("continue")
+    elif choice == 'N':
+        print("exiting")
+        break

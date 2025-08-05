0806b007b8843dde4fb36f7b9bab412f665dfc7e
talhadevstree <talha.s@devstree.in>
2025-08-05 17:31:11 +0530

    Add files via upload

---
+++ Week2/W2Day2/setoperations.py
@@ -0,0 +1,10 @@
+math = {"alice","bob", "john", "rahul","jay"}
+science={"rahul" , "john" ,"man", "zub" , "jack"}
+
+union = math.union(science)
+difference = math.difference(science)
+intersection = math.intersection(science)
+
+print("union",union)
+print("difference",difference)
+print("intersection",intersection)

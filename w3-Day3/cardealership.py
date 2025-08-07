b963f1688998493ce4b418bca3eeb0ea3bdd4819
talhadevstree <talha.s@devstree.in>
2025-08-07 16:14:30 +0530

    Car Dealership

--- w3-Day3/cardealership.py
+++ w3-Day3/cardealership.py
@@ -17,7 +17,7 @@ class Dealership:
     def add_car(self , name , model , year , price):
          car = Car(name , model , year , price)
          self.inventory.append(car)
-         print("Inventory  Added")
+         print("Inventory Added")
          print('\n')
 
     def display(self):
@@ -25,15 +25,15 @@ class Dealership:
             print(f"---------Inventory of {self.name}-----------")
             for car in self.inventory:
                 print(car.car_info())
-            print(f"----------------Thanks You----------------")
+            print(f"----------------Thanks You-----------------")
         else:
             print('\n')
             print("Inventory is empty")
     
 carn = input("Car Name :")
 carm = input("Car Model :")
-cary =input("Car Year :")
-carp =input("Car Price :")
+cary = input("Car Year :")
+carp = input("Car Price :")
 
 obj2 = Dealership("Autoworld")
 

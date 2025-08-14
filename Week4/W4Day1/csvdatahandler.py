315a68dff2ceb5c905cc976aaf335d366abd7fcd
talhadevstree <talha.s@devstree.in>
2025-08-14 10:08:02 +0530

    File Operations

---
+++ Week4/W4Day1/csvdatahandler.py
@@ -0,0 +1,26 @@
+# Read and write CSV files with student data, calculate averages
+import csv
+
+with open('file/student.csv', 'a') as f:
+     f.write("Name,Math,Science,English\n")
+     f.write("John,85,90,78\n")
+     f.write("Dev,92,88,84\n")
+     f.write("jay,75,80,70\n")
+     f.write("Bob,89,94,91\n")
+  
+
+with open ('file/student.csv' ,'r') as r:
+    content = csv.reader(r)
+    next(content)
+    for row in content:
+        name = row[0].strip()
+        math = int(row[1].strip())
+        science = int(row[2].strip())
+        english = int(row[3].strip())
+
+        avg = (math + science + english) / 3  
+        print(f"{name}'s Average is: {avg:.2f}")
+
+
+
+

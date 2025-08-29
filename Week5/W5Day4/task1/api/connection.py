7e5e07d2ce45e13555fe4a355fcef29b457809ad
talhadevstree <talha.s@devstree.in>
2025-08-29 09:19:27 +0530

    Delete Week5 directory

--- Week5/W5Day4/task1/api/connection.py
+++
@@ -1,9 +0,0 @@
-import requests
-from api import weatherapi
-
-city = "london"
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weatherapi}&units=metric"
-
-response = requests.get(url)
-data = response.json()
-print(data)
